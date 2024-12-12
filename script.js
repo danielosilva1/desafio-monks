2fd202692b46dbc69df9b800386ff16ecddc83eb
dosilva <danielosilva@alu.ufc.br>
2024-12-12 16:37:13 -0300

    inicia implementação da verificação de segurança: escolhe dois números aleatórios e mostra-os para o usuário na caixa de verificação

--- script.js
+++ script.js
@@ -1,85 +1,108 @@
+let menuIsOpen = false;
+// Números que serão somados na verificação de segurança
+let number1 = -1;
+let number2 = -1;
+
+// Lida com a abertura e fechamento do menu (quando está na visualização mobile)
+function handleMenuView() {
+    if (menuIsOpen) {
+        closeVerticalMenu();
+        menuIsOpen = !menuIsOpen;
+    } else {
+        openVerticalMenu();
+        menuIsOpen = !menuIsOpen;
+    }
+}
+
+// Altera estilo para mostrar menu na vertical
+function openVerticalMenu() {
+    // Altera cor do contanier de apresentação
+    document.getElementsByClassName("container-presentation")[0].style.backgroundColor = "rgba(223, 187, 254, 1)";
+
+    // Altera raio das bordas inferiores do header que contém logo e ícone de menu
+    const headerLogoMenuIcon = document.getElementsByClassName("header-logo-menu-icon")[0];
+    headerLogoMenuIcon.style.borderBottomLeftRadius = "16px";
+    headerLogoMenuIcon.style.borderBottomRightRadius = "16px";
+
+    // Oculta imagem com as logos
+    document.getElementsByClassName("container-presentation__logos-img")[0].style.display = "none";
+
+    // Oculta a div com as informações (título e descrição)
+    document.getElementsByClassName("header-info")[0].style.display = "none";
+
+    // Mostra categorias abaixo da logo e do botão de menu
+    document.getElementsByClassName("header")[0].style.display = "block";
+
+    // Mostra categorias na vertical
+    const headerCategories = document.getElementsByClassName("header-categories")[0];
+
+    headerCategories.style.display = "flex";
+    headerCategories.style.flexDirection = "column";
+
+    // Altera cor do texto das categorias
+    const categoriesList = document.getElementsByClassName("header-categories__category");
+    for (let i = 0; i < categoriesList.length; i++) {
+        categoriesList[i].style.color = "black";
+    }
+
+    // Mostrar back-img
+    document.getElementsByClassName("header-categories__back-img")[0].style.display = "block";
+}
+
+// Reverte as alterações de estilo provocadas pela exibição do menu vertical
+function closeVerticalMenu() {
+    document.getElementsByClassName("container-presentation")[0].style.backgroundColor = "";
+
+    const headerLogoMenuIcon = document.getElementsByClassName("header-logo-menu-icon")[0];
+    headerLogoMenuIcon.style.borderBottomLeftRadius = "";
+    headerLogoMenuIcon.style.borderBottomRightRadius = "";
+
+    document.getElementsByClassName("container-presentation__logos-img")[0].style.display = "";
+
+    document.getElementsByClassName("header-info")[0].style.display = "";
+
+    document.getElementsByClassName("header")[0].style.display = "";
+
+    const headerCategories = document.getElementsByClassName("header-categories")[0];
+    headerCategories.style.display = "";
+    headerCategories.style.flexDirection = "";
+
+    const categoriesList = document.getElementsByClassName("header-categories__category");
+
+    for (let i = 0; i < categoriesList.length; i++) {
+        categoriesList[i].style.color = "";
+    }
+
+    document.getElementsByClassName("header-categories__back-img")[0].style.display = "";
+}
+
+function checkWindowWidth() {
+    if (window.innerWidth > 768) {
+        // Restaura o estilo para o padrão: visualização em desktop
+        closeVerticalMenu();
+    }
+}
+
+// Função retorna um número aleatório entre 1 e 1000
+function getAleatoryNumber() {
+    return Math.floor(Math.random() * 1000 + 1);
+}
+
+// Função obtém dois números aleatórios x e y e adicione "x + y" à caixa de texto
+function setSumNumbers() {
+    number1 = getAleatoryNumber();
+    number2 = getAleatoryNumber();
+
+    console.log(`${number1} + ${number2}`);
+
+    // security_area_sum.
+    document.getElementsByClassName("security_area__sum")[0].value = `${number1} + ${number2}`;
+}
+
 // Garante que script será executado após carregamento do DOM
 document.addEventListener("DOMContentLoaded", function () {
-    let menuIsOpen = false;
-
-    function handleMenuView() {
-        if (menuIsOpen) {
-            closeVerticalMenu();
-            menuIsOpen = !menuIsOpen;
-        } else {
-            openVerticalMenu();
-            menuIsOpen = !menuIsOpen;
-        }
-    }
-
-    // Altera estilo para mostrar menu na vertical
-    function openVerticalMenu() {
-        // Altera cor do contanier de apresentação
-        document.getElementsByClassName("container-presentation")[0].style.backgroundColor = "rgba(223, 187, 254, 1)";
-
-        // Altera raio das bordas inferiores do header que contém logo e ícone de menu
-        const headerLogoMenuIcon = document.getElementsByClassName("header-logo-menu-icon")[0];
-        headerLogoMenuIcon.style.borderBottomLeftRadius = "16px";
-        headerLogoMenuIcon.style.borderBottomRightRadius = "16px";
-
-        // Oculta imagem com as logos
-        document.getElementsByClassName("container-presentation__logos-img")[0].style.display = "none";
-
-        // Oculta a div com as informações (título e descrição)
-        document.getElementsByClassName("header-info")[0].style.display = "none";
-
-        // Mostra categorias abaixo da logo e do botão de menu
-        document.getElementsByClassName("header")[0].style.display = "block";
-
-        // Mostra categorias na vertical
-        const headerCategories = document.getElementsByClassName("header-categories")[0];
-
-        headerCategories.style.display = "flex";
-        headerCategories.style.flexDirection = "column";
-
-        // Altera cor do texto das categorias
-        const categoriesList = document.getElementsByClassName("header-categories__category");
-        for (let i = 0; i < categoriesList.length; i++) {
-            categoriesList[i].style.color = "black";
-        }
-
-        // Mostrar back-img
-        document.getElementsByClassName("header-categories__back-img")[0].style.display = "block";
-    }
-
-    // Reverte as alterações de estilo provocadas pela exibição do menu vertical
-    function closeVerticalMenu() {
-        document.getElementsByClassName("container-presentation")[0].style.backgroundColor = "";
-
-        const headerLogoMenuIcon = document.getElementsByClassName("header-logo-menu-icon")[0];
-        headerLogoMenuIcon.style.borderBottomLeftRadius = "";
-        headerLogoMenuIcon.style.borderBottomRightRadius = "";
-
-        document.getElementsByClassName("container-presentation__logos-img")[0].style.display = "";
-
-        document.getElementsByClassName("header-info")[0].style.display = "";
-
-        document.getElementsByClassName("header")[0].style.display = "";
-
-        const headerCategories = document.getElementsByClassName("header-categories")[0];
-        headerCategories.style.display = "";
-        headerCategories.style.flexDirection = "";
-
-        const categoriesList = document.getElementsByClassName("header-categories__category");
-
-        for (let i = 0; i < categoriesList.length; i++) {
-            categoriesList[i].style.color = "";
-        }
-
-        document.getElementsByClassName("header-categories__back-img")[0].style.display = "";
-    }
-
-    function checkWindowWidth() {
-        if (window.innerWidth > 768) {
-            // Restaura o estilo para o padrão: visualização em desktop
-            closeVerticalMenu();
-        }
-    }
+    
+    setSumNumbers();
 
     document.getElementsByClassName("header-categories__menu-icon")[0].addEventListener("click", handleMenuView);
     document.getElementsByClassName("header-categories__back-img")[0].addEventListener("click", closeVerticalMenu);
